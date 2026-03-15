46a1bca24239686b5cfbcc515ed07f0bafa772a7
satish sahu <159688963+satish191821@users.noreply.github.com>
2026-03-15 15:23:38 +0530

    GeekHub v2:- jai ho

--- Basic/Array Search/array-search.java
+++ Basic/Array Search/array-search.java
@@ -1,6 +1,6 @@
 class Solution {
     public int search(int arr[], int x) {
-        // code  ghfg gdfgrt yt
+        // code  ghfg gdfgrfgetet yt
         for(int i=0;i<arr.length;i++){
             if(arr[i]==x){
                 return i;
