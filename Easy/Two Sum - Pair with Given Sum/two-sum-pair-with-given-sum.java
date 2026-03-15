19e4350c98f41711e034968be4447e86370d660f
satish sahu <159688963+satish191821@users.noreply.github.com>
2026-03-15 14:53:19 +0530

    Update Solution: Two Sum - Pair with Given Sum [.java] (auto) @ 2026-03-15 09:23

---
+++ Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        // code here rehytgutyt uify4edrterrtytyrtyrf
+        int n = arr.length;
+        HashSet <Integer> set = new HashSet<>();
+        
+        for(int i=0;i<arr.length;i++){
+            int comp = target - arr[i];
+            if(set.contains(comp)){
+                return true;
+            }
+                set.add(arr[i]);
+            
+        }
+        return false;
+    }
+}
